5944796e1f0977a9a3ec88df8766f60fa0ead712
carl0801 <118434635+carl0801@users.noreply.github.com>
2025-03-02 15:08:56 +0100

    add firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,20 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgi-7M-X8NQ_0HG1urQphS0982KrFldsc",
+  authDomain: "lemur-c8d6b.firebaseapp.com",
+  projectId: "lemur-c8d6b",
+  storageBucket: "lemur-c8d6b.appspot.com",
+  messagingSenderId: "319892895415",
+  appId: "1:319892895415:web:42d10b09975a8f8ddc7f32",
+  measurementId: "G-8GPEHHFGK3"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
