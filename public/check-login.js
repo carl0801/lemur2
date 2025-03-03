cc17675da94ea91e7ad3dafc37244e2aa4ed65f0
carl0801 <118434635+carl0801@users.noreply.github.com>
2025-03-03 15:59:16 +0100

    d

--- public/check-login.js
+++ public/check-login.js
@@ -1,7 +1,10 @@
 
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
 const auth = getAuth();
+const db = getDatabase();
+
 
 // Fetch user data from the Realtime Database
 function fetchUserData(userId) {
