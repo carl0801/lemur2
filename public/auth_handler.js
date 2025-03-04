b57322ee5c18cb386f36fa4410c67103841e02f6
carl0801 <118434635+carl0801@users.noreply.github.com>
2025-03-04 08:56:07 +0100

    d

--- public/auth_handler.js
+++ public/auth_handler.js
@@ -1,13 +1,6 @@
 
 import { auth, getAuth, updateProfile, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from './firebase-config.js';
 
-// Log out the user when they leave the page
-window.addEventListener("beforeunload", () => {
-    signOut(auth).catch((error) => {
-      console.error("Error logging out:", error);
-    });
-  });
-
 
 // Example of a logout function
 function logout() {
